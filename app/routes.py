41fe0ab83d9e354fd7bab9f85e86a8a52ca081f6
Jorge <jjmmc@hotmail.com>
2025-05-31 19:45:57 +0200

    routes

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends        # APIRouter para definir rutas, Depends para inyección de dependencias
+from fastapi import APIRouter, Depends, HTTPException        # APIRouter para definir rutas, Depends para inyección de dependencias
 from sqlalchemy.orm import Session           # Session para manejar la sesión de la base de datos
 import app.models  as models                              # Tus modelos de SQLAlchemy (Task)
 import app.schemas as schemas                               # Tus esquemas de Pydantic (Task, TaskCreate)
@@ -22,6 +22,23 @@ def create_task(task: schemas.TaskCreate, db: Session = Depends(get_db)):
     db.refresh(db_task)
     return db_task
 
+@router.get("/tasks/", response_model=list[schemas.Task])
+def get_tasks(db: Session = Depends(get_db)):
+    return db.query(models.Task).all()
 
+@router.get("/tasks/{user_id}")
+def get_task_by_id(task_id: int, db: Session = Depends(get_db)):
+    task = db.query(models.Task).filter(models.Task.id == task_id).first()
+    if task is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return task
 
-
+@router.put('/tasks/')
+def update_task( updated_task: schemas.TaskCreate, task_id: int, db: Session = Depends(get_db)):
+    task = db.query(models.Task).filter(models.Task.id == task_id).first()
+    if not task:
+        raise HTTPException(status_code=404, detail="Task not found")
+    for key, value in updated_task.model_dump().items():
+        setattr(task, key, value)
+    db.commit()
+    return db.query(models.Task).filter(models.Task.id == task_id).first()
